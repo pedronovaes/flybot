a0813e207eb2559dd11b5768e28e01fa0494d62f
pedronovaes <pedronovaesmelo@gmail.com>
2025-09-30 20:50:50 -0300

    Add tool to search for trip recommendations

--- flybot/tools/excursions.py
+++ flybot/tools/excursions.py
@@ -12,6 +12,49 @@ from langchain_core.runnables import RunnableConfig
 
 
 @tool
+def search_trip_recommendations(
+    config: RunnableConfig,
+    location: Optional[str] = None,
+    name: Optional[str] = None,
+    keywords: Optional[str] = None
+) -> list[dict]:
+    """
+    Search for trip recommendations based on location, name, and keywords.
+    """
+
+    configuration = config.get('configurable', {})
+    db = configuration.get('db', '')
+
+    conn = sqlite3.connect(database=db)
+    cursor = conn.cursor()
+
+    query = "select * from trip_recommendations where 1 = 1"
+    params = []
+
+    if location:
+        query += " and location like ?"
+        params.append(f"%{location}%")
+
+    if name:
+        query += " and name like ?"
+        params.append(f"%{name}%")
+
+    if keywords:
+        keywords_list = keywords.split(',')
+        keywords_conditions = ' or '.join(["keywords like ?" for _ in keywords_list])
+
+        query += f" and ({keywords_conditions})"
+        params.extend([f"%{keyword.strip()}%" for keyword in keywords_list])
+
+    cursor.execute(query, params)
+    rows = cursor.fetchall()
+    column_names = [column[0] for column in cursor.description]
+    results = [dict(zip(column_names, row)) for row in rows]
+
+    return results
+
+
+@tool
 def book_excursion(recommendation_id: int, config: RunnableConfig) -> str:
     """
     Book an excursion by its recommendation ID.
